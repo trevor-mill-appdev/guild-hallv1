63f4b18dabede40a98804b0fa15a4237addc3b5c
trevor-mill <trevor.million@chicagobooth.edu>
2022-05-24 18:15:27 +0000

    user associations added

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -1,3 +1,5 @@
 class Guild < ApplicationRecord
   belongs_to :admin, class_name: "User"
+
+  has_many :bulletins
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :stashes
+  has_many :bulletins, { foreign_key: :author_id, dependent: :nullify }
+  has_many :guilds, { foreign_key: :admin_id }
+  has_many :raiders, { foreign_key: :owner_id, dependent: :destroy }
+  has_many :proposals, { foreign_key: :proposer_id, dependent: :nullify }
+  has_many :votes, { foreign_key: :voter_id, dependent: :nullify }
+  has_many :mobs, { foreign_key: :owner_id, dependent: :destroy }
+
+  belongs_to :guild, { foreign_key: :guild_id, counter_cache: true }
+  
 end
