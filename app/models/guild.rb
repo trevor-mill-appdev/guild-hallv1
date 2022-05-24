f2b71b7c74c820b034a7907c7cc269a3182171f7
trevor-mill <trevor.million@chicagobooth.edu>
2022-05-24 18:22:43 +0000

    guild associations added

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -1,5 +1,8 @@
 class Guild < ApplicationRecord
   belongs_to :admin, class_name: "User"
 
-  has_many :bulletins
+  belongs_to :owner, required: true, class_name: "User", foreign_key: :admin_id
+  has_many :bulletins, foreign_key: :guild_id, dependent: :nullify
+  has_many :users, foreign_key: :guild_id, dependent: :nullify
+  has_many :proposals, foreign_key: :guild_id, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :stashes
-  has_many :bulletins, { foreign_key: :author_id, dependent: :nullify }
-  has_many :guilds, { foreign_key: :admin_id }
-  has_many :raiders, { foreign_key: :owner_id, dependent: :destroy }
-  has_many :proposals, { foreign_key: :proposer_id, dependent: :nullify }
-  has_many :votes, { foreign_key: :voter_id, dependent: :nullify }
-  has_many :mobs, { foreign_key: :owner_id, dependent: :destroy }
+  has_many :bulletins, foreign_key: :author_id, dependent: :nullify 
+  has_many :guilds, foreign_key: :admin_id
+  has_many :raiders, foreign_key: :owner_id, dependent: :destroy
+  has_many :proposals, foreign_key: :proposer_id, dependent: :nullify
+  has_many :votes, foreign_key: :voter_id, dependent: :nullify 
+  has_many :mobs, foreign_key: :owner_id, dependent: :destroy 
 
-  belongs_to :guild, { foreign_key: :guild_id, counter_cache: true }
+  belongs_to :guild, foreign_key: :guild_id, counter_cache: true
   
 end
