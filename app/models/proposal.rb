abfa6d40c479e1a098818d6e141c9b3dbf93af01
trevor-mill <trevor.million@chicagobooth.edu>
2022-05-24 19:27:45 +0000

    validations and relations v1 complete

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,4 +1,7 @@
 class Proposal < ApplicationRecord
+
+  validates :status, inclusion: { in: %w(pending denied passed)}
+
   belongs_to :proposer, required: true, class_name: "User", foreign_key: :proposer_id
   belongs_to :guild, required: true
   has_many :votes
