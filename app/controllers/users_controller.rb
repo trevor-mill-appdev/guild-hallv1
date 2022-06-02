0ebe20bdba0d4a0cd72154ed178fb297f60b539f
trevor-mill <trevor.million@chicagobooth.edu>
2022-06-02 04:25:35 +0000

    finished /inbox logic and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
   end
 
   def inbox
+    @needs_vote = Array.new
+
+    proposals = current_user.guild.proposals.where(:status => "pending").all
+
+    proposals.each do |proposal|
+      has_voted = current_user.votes.where(:proposal_id => proposal.id).first
+
+      if !has_voted
+        @needs_vote << proposal    
+      end
+
+    end
   end
 
 end
